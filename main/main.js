0e1f60b3a44b61f46d6af85125d0ec1de1b9bf48
박상지 <2017069598@hanyang.ac.kr>
2021-07-27 19:45:45 +0900

    nodejs 기본적인 서버 구현

---
+++ main/main.js
@@ -0,0 +1,28 @@
+var http = require('http');
+
+//웹 서버 객체를 만듬.
+var server = http.createServer();
+
+var port = 3000;
+
+//웹서버를 시작하여 3000번 포트에서 대기하도록 설정.
+server.listen(port, function(){
+    console.log('웹 서버가 시작되었습니다. : %d ' ,port);
+});
+
+//클라이언트 요청 이벤트 처린
+server.on('connection',function(socket){
+    var addr = socket.address();
+    console.log('클라이언트가 접속했습니다. : %s, %d',addr.address,addr.port);
+});
+
+//클라이언트 요청 이벤트 처리
+server.on('request',function(req,res){
+    console.log('클라이언트 요청이 들어왔습니다.');
+    console.log(req);
+});
+
+// 서버 종료 이벤트 처리.
+server.on('close',function(){
+    console.log('서버가 종료됩니다.');
+});
